cc63044f1e07c0e11408d2515f69d1a201f45c20
Quinten Steenhuis <qsteenhuis@gmail.com>
2018-10-30 20:11:49 +0000

    Initial version - handles basic financial statement

---
+++ README.md
@@ -0,0 +1,8 @@
+# SSI Overpayment waiver
+
+An interview to help an individual appeal an overpayment set by the Social
+Security Administration.
+
+# Changelog
+
+* Initial version - handles basic financial statement
---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup, find_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.py', '*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.ssioverpaymentwaiver',
+      version='0.0.1',
+      description=('Request for Waiver of SSI Overpayment'),
+      long_description='# SSI Overpayment waiver\r\n\r\nAn interview to help an individual appeal an overpayment set by the Social\r\nSecurity Administration.\r\n\r\n# Changelog\r\n\r\n* Initial version - handles basic financial statement',
+      long_description_content_type='text/markdown',
+      author='Quinten Steenhuis',
+      author_email='qsteenhuis@gmail.com',
+      license='The MIT License',
+      url='https://docassemble.org',
+      packages=find_packages(),
+      namespace_packages=['docassemble'],
+      install_requires=[],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/ssioverpaymentwaiver/', package='docassemble.ssioverpaymentwaiver'),
+     )
+
