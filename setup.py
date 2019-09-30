e5814a019f87d84d1fafd3072582f60ee8df70b0
Quinten Steenhuis <qsteenhuis@gmail.com>
2019-09-29 21:25:15 -0400

    Empty currency

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.ssioverpaymentwaiver',
-      version='0.0.15',
+      version='0.0.16',
       description=('Request for Waiver of SSI Overpayment'),
       long_description='# SSI Overpayment waiver\r\n\r\nAn interview to help an individual appeal an overpayment set by the Social\r\nSecurity Administration.\r\n\r\n# Changelog\r\n\r\n* 2019-09-29 Bug fixes (question order, hide irrelevant questions)\r\n* 2019-09-21 Bug fixes, finish implementing PDF fields\r\n* 2019-02-06 Preliminary mapping of all PDF fields\r\n* 2019-01-25 Bring to current\r\n* 2018-12-01 Improved financial statement (in progress) and logical questions for overpayment\r\n* Initial version - handles basic financial statement',
       long_description_content_type='text/markdown',
